41ef4fb31b050666d50e0f9f74b32c81e1e3d90d
Sanzio Monti <github@sanziomonti.xyz>
2024-01-13 23:08:21 +0100

    Update image loading and animation duration

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -4,7 +4,12 @@ import "../styles/HomePage.css";
 const HomePage: React.FC = () => {
   return (
     <div className="homepage">
-      <img className="glassenz" src="images/glassenz.webp" alt="Sanzio 30" />
+      <img
+        className="glassenz"
+        src="images/glassenz.webp"
+        alt="Sanzio 30"
+        loading="lazy"
+      />
     </div>
   );
 };
